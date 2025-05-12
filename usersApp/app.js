933c6619f5871ac5c894c46e362d3c40388d1af5
pvergoulis <vergoulispaulos@gmail.com>
2025-05-12 21:07:13 +0300

    email checked

--- usersApp/app.js
+++ usersApp/app.js
@@ -15,8 +15,8 @@ const userProduct = require('./routes/user.product.route')
 const auth = require('./routes/auth.routes')
 
 app.use(cors({
- // origin : "*"
-  origin : ['http://localhost:3000']
+ origin : "*"
+  // origin : ['http://localhost:3000']
 }))
 
 app.use('/api/users', user)
--- usersApp/routes/user.routes.js
+++ usersApp/routes/user.routes.js
@@ -8,9 +8,11 @@ const verifyRoles = require('../middlewares/auth.middleware').verifyRoles
 
 router.get('/',verifyToken,userController.findAll)
 router.get('/:username',verifyToken, userController.findOne)
-router.post('/', verifyToken, verifyRoles("ADMIN"), userController.create)
+router.post('/', userController.create)
+// router.post('/', verifyToken, verifyRoles("ADMIN"), userController.create)
 router.patch('/:username', verifyToken, verifyRoles("ADMIN"), userController.update)
 router.delete('/:username', verifyToken, verifyRoles("ADMIN"), userController.deleteByUsername)
 router.delete('/:username/email/:email',verifyToken, verifyRoles("ADMIN"),  userController.deleteByEmail)
 
+router.get('/check_duplicate_email/:email',userController.checkDuplicateEmail)
 module.exports = router
